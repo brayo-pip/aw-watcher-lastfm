66e6852705644cf1743d9b7dbe197b91ea269642
brayo <vukubrian@gmail.com>
2024-04-10 20:48:20 +0300

    feat: post events to aw-server-rust

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use reqwest;
-use serde_json::Value;
+use serde_json::{Value, Map};
 use serde_yaml;
+use tokio::time::sleep;
 use std::fs::File;
 use std::io::prelude::*;
+use aw_client_rust::AwClient;
+use aw_models::Event;
+use chrono::{TimeDelta, Utc};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -18,14 +22,30 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let url = format!("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={}&api_key={}&format=json", username, apikey);
 
-    let response = reqwest::get(&url).await?.text().await?;
+    let aw_client = AwClient::new("localhost", "5600", "aw-firebase-sync");
+    // creates a new bucket if it doesn't exist, otherwise does nothing
+    aw_client.create_bucket("aw-watcher-lastfm", "lastfm_events").unwrap();
+    
+    loop {
+        let response = reqwest::get(&url).await?.text().await?;
 
-    let v: Value = serde_json::from_str(&response)?;
+        let v: Value = serde_json::from_str(&response)?;
 
-    match v["recenttracks"]["track"][0]["@attr"]["nowplaying"].as_str() {
-        Some("true") => println!("User is currently listening to: {}", v["recenttracks"]["track"][0]["name"]),
-        _ => println!("User is not listening to anything right now"),
+        if v["recenttracks"]["track"][0]["@attr"]["nowplaying"].as_str() != Some("true") {
+            sleep(std::time::Duration::from_secs(1)).await;
+            continue;
+        }
+        let mut data = Map::new();
+        data.insert("track".to_string(), v["recenttracks"]["track"][0]["name"].clone());
+        data.insert("artist".to_string(), v["recenttracks"]["track"][0]["artist"]["#text"].clone());
+        data.insert("album".to_string(), v["recenttracks"]["track"][0]["album"]["#text"].clone());
+        let event = Event {
+            id: None,
+            timestamp: Utc::now(),
+            duration: TimeDelta::seconds(1),
+            data: data,
+        };
+        aw_client.heartbeat("aw-watcher-lastfm", &event, 5.0).unwrap();
+        sleep(std::time::Duration::from_secs(1)).await;
     }
-
-    Ok(())
 }
