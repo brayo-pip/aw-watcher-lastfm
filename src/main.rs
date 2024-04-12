72dcac1f5c3035394e05d8ea558c71a056df4559
brayo <vukubrian@gmail.com>
2024-04-12 09:50:19 +0300

    reuse sessions, add a bit of formatting

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,46 @@
-use reqwest;
-use serde_json::{Value, Map};
-use serde_yaml;
-use tokio::time::sleep;
-use std::fs::{File, DirBuilder};
-use std::io::prelude::*;
 use aw_client_rust::AwClient;
-use aw_models::{Event, Bucket};
+use aw_models::{Bucket, Event};
 use chrono::{TimeDelta, Utc};
 use dirs::config_dir;
+use reqwest;
+use serde_json::{Map, Value};
+use serde_yaml;
+use std::fs::{DirBuilder, File};
+use std::io::prelude::*;
+use tokio::time::sleep;
 
 fn get_config_path() -> Option<std::path::PathBuf> {
-  config_dir().map(|mut path| {
-    path.push("aw-watcher-lastfm");
-    path.push("config.yaml");
-    path
-  })
+    config_dir().map(|mut path| {
+        path.push("aw-watcher-lastfm");
+        path.push("config.yaml");
+        path
+    })
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config_path = get_config_path().expect("Unable to get config path");
-    
+
     if !config_path.exists() {
-        DirBuilder::new().recursive(true).create(config_path.parent().unwrap()).expect("Unable to create directory");
+        DirBuilder::new()
+            .recursive(true)
+            .create(config_path.parent().unwrap())
+            .expect("Unable to create directory");
         let mut file = File::create(&config_path).expect("Unable to create file");
-        file.write_all(b"apikey: your-api-key\nusername: your_username\npolling_interval: 10").expect("Unable to write to file");
-        println!("Please set your api key and username at {:?}", config_path.clone());
+        file.write_all(b"apikey: your-api-key\nusername: your_username\npolling_interval: 10")
+            .expect("Unable to write to file");
+        println!(
+            "Please set your api key and username at {:?}",
+            config_path.clone()
+        );
         return Ok(());
     }
 
     let mut file = File::open(config_path.clone()).expect("Unable to open file");
     let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("Unable to read file");
-    
+    file.read_to_string(&mut contents)
+        .expect("Unable to read file");
+
     let yaml: Value = serde_yaml::from_str(&contents).unwrap();
     let apikey = yaml["apikey"].as_str().unwrap().to_string();
     let username = yaml["username"].as_str().unwrap().to_string();
@@ -50,18 +58,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let aw_client = AwClient::new("localhost", 5600, "aw-watcher-lastfm").unwrap();
     // creates a new bucket if it doesn't exist, otherwise does nothing
-    aw_client.create_bucket(&Bucket {
-        id: "lastfm".to_string(),
-        bid: None,
-        _type: "currently-playing".to_string(),
-        data: Map::new(),
-        metadata: Default::default(),
-        last_updated: None,
-        hostname: "".to_string(),
-        client: "aw-watcher-lastfm".to_string(),
-        created: None,
-        events: None,
-    }).await.unwrap();
+    aw_client
+        .create_bucket(&Bucket {
+            id: "lastfm".to_string(),
+            bid: None,
+            _type: "currently-playing".to_string(),
+            data: Map::new(),
+            metadata: Default::default(),
+            last_updated: None,
+            hostname: "".to_string(),
+            client: "aw-watcher-lastfm".to_string(),
+            created: None,
+            events: None,
+        })
+        .await
+        .unwrap();
 
     let polling_duration = std::time::Duration::from_secs(polling_interval as u64);
     let polling_time = TimeDelta::seconds(polling_interval);
@@ -73,17 +84,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             sleep(std::time::Duration::from_secs(polling_interval as u64)).await;
             continue;
         }
-        let mut data = Map::new();
-        data.insert("track".to_string(), v["recenttracks"]["track"][0]["name"].to_owned());
-        data.insert("artist".to_string(), v["recenttracks"]["track"][0]["artist"]["#text"].to_owned());
-        data.insert("album".to_string(), v["recenttracks"]["track"][0]["album"]["#text"].to_owned());
+        let mut event_data = Map::new();
+        event_data.insert(
+            "track".to_string(),
+            v["recenttracks"]["track"][0]["name"].to_owned(),
+        );
+        event_data.insert(
+            "artist".to_string(),
+            v["recenttracks"]["track"][0]["artist"]["#text"].to_owned(),
+        );
+        event_data.insert(
+            "album".to_string(),
+            v["recenttracks"]["track"][0]["album"]["#text"].to_owned(),
+        );
         let event = Event {
             id: None,
             timestamp: Utc::now(),
             duration: polling_time,
-            data: data,
+            data: event_data,
         };
-        aw_client.heartbeat("aw-watcher-lastfm", &event, polling_interval as f64).await.unwrap();
+        aw_client
+            .heartbeat("aw-watcher-lastfm", &event, polling_interval as f64)
+            .await
+            .unwrap();
         sleep(polling_duration).await;
     }
 }
